a46ebd4320bc39ad2ef936aba1c5e1ee90fc4197
Brad Carlson <b2carlso@yahoo.com>
2018-05-25 19:39:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function justInvoke(fn){
+function justInvoke(fn) {
   return fn();
 }
 
-function setThisWithCall(fn, thisValue, arg) {
+function setThisWithCall(fn, thisValue, arg){
+  return fn.call(thisValue, arg);
+}
 
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args);
+}
+
+function returnNewFunctionOf(functionToBeCopied, thisValue){
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
 }
